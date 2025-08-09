b527a2c4d281c11e46b9db57f6ea4b967a017c54
2qp <56475797+2qp@users.noreply.github.com>
2025-08-09 22:04:48 +0530

    feat(data): add `colors` on `COLOR_MAP`

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,6 +4,9 @@ const COLOR_MAP = {
   blue: "bg-blue-500 text-white",
   yellow: "bg-yellow-400 text-black",
   gray: "bg-neutral-6",
+  "task-lime-green": "bg-task-lime-green text-black",
+  "task-yellow": "bg-task-yellow text-black",
+  "task-red": "bg-task-red text-black",
   // fallback
   default: "bg-gray-300 text-black",
 } as const;
